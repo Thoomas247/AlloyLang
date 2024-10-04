ecef905fa57ae9a498ed13891d6daf2ca17bd5a7
Thomas <thomas@amyuni.com>
2024-10-04 10:58:18 +0100

    Fixed parser bug where function calls weren't considered statements.

--- AlloyLang/compiler/module/node/NodeBuffer.cpp
+++ AlloyLang/compiler/module/node/NodeBuffer.cpp
@@ -2637,6 +2637,10 @@ namespace AlloyCompiler
 			{
 				pStatement = createNode(STATEMENT(pExpression->Get<FUNCTION_CALL>()));
 			}
+			else if (pExpression->Is<PRIMARY>() && pExpression->Get<PRIMARY>()->Is<FUNCTION_CALL>())
+			{
+				pStatement = createNode(STATEMENT(pExpression->Get<PRIMARY>()->Get<FUNCTION_CALL>()));
+			}
 			else if (pExpression->Is<POSTFIX>() && pExpression->Get<POSTFIX>()->Is<FUNCTION_CALL>())
 			{
 				pStatement = createNode(STATEMENT(pExpression->Get<POSTFIX>()->Get<FUNCTION_CALL>()));
